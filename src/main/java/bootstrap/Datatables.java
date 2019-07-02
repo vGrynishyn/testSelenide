8153fd8ee8a384717262af824934b93e9f38bb9b
vhrynishyn <vgrinishin@gmail.com>
2019-07-02 19:07:28 +0300

    changes

--- src/main/java/bootstrap/Datatables.java
+++ src/main/java/bootstrap/Datatables.java
@@ -1,8 +1,10 @@
 package bootstrap;
 
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.codeborne.selenide.Selenide.$$;
@@ -13,10 +15,15 @@ public class Datatables {
     public boolean searchValyeInColumnTable(String columnName, String value){
         int columnNumber = getAllColumn(columnName);
         // $$x(".//*[@id='dtBasicExample']/tbody/tr").get(0).$$x("td").get(0).text()
-        ElementsCollection columnValues = $$x(".//*[@id='dtBasicExample']/tbody/tr"); ///td["+ columnNumber +"]");
+        ElementsCollection columnValuesColl = $$x(".//*[@id='dtBasicExample']/tbody/tr"); ///td["+ columnNumber +"]");
+        List<String> valuesList = new ArrayList<>();
+        for (SelenideElement element : columnValuesColl ) {
+            valuesList.add(element.$$x("td").get(columnNumber).text());
+        }
 
-        List<String> columnValues = columnValues.forEach(x -> x.);
-        return columnValues.texts().contains(value);
+//        List<SelenideElement> tr = columnValuesColl.forEach(row -> row.$$x("td"));
+//        return columnValues.texts().contains(value);
+        return true;
 
     }
 
